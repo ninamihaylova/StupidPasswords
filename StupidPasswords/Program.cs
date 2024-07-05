af92b70a7374047f0bed1fc571ffe12f53f800e2
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-05 18:31:20 +0300

    Initial commit

---
+++ StupidPasswords/Program.cs
@@ -0,0 +1,17 @@
+﻿class StupidPasswords
+{
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int firstDigit = 2; firstDigit <= number; firstDigit += 2)
+        {
+            for (int secondDigit = 1; secondDigit <= number; secondDigit += 2)
+            {
+                int thirdDigit = firstDigit * secondDigit;
+                
+                Console.Write($"{firstDigit}{secondDigit}{thirdDigit} ");
+            }
+        }
+    }
+}
